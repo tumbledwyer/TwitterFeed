993ba3c79568e4b96540a718b02b9e20ba9cf66a
Barry Dwyer <dwyerbaz@gmail.com>
2017-01-29 14:09:27 +0400

    Dirty user implementation

--- source/TwitterFeed/TwitterApp.cs
+++ source/TwitterFeed/TwitterApp.cs
@@ -22,23 +22,64 @@ namespace TwitterFeed
                 throw new ArgumentException("Method requires 2 file paths");
             }
 
-            var users = new List<string>();
             var userLines = File.ReadAllLines(filePaths[0]).OrderBy(s => s).ToList();
-            userLines.ForEach(s => users.AddRange(s.Split(new [] {" follows"}, StringSplitOptions.RemoveEmptyEntries)));
+
+            var users = CreateUsers(userLines);
 
             var tweetLines = File.ReadAllLines(filePaths[1]);
             var tweets = tweetLines.Select(CreateTweet).ToList();
             
-            users.ForEach(s =>
+            users.ForEach(u =>
             {
-                _logger.Log(s);
+                _logger.Log(u.Name);
                 tweets.ForEach(t =>
                 {
-                    if (t.StartsWith($"\t@{s}"))
+                    if (t.StartsWith($"\t@{u.Name}"))
                         _logger.Log(t);
+                    u.Following.ForEach(f =>
+                    {
+                        if (t.StartsWith($"\t@{f.Name}"))
+                            _logger.Log(t);
+                    });
                 });
             });
             
+        }
+
+        private List<User> CreateUsers(List<string> userLines)
+        {
+            var users = new List<User>();
+            userLines.ForEach(s =>
+            {
+                var userNames = s.Split(new[] {" follows"}, StringSplitOptions.RemoveEmptyEntries);
+                var primary = userNames[0];
+                var followers = userNames.Skip(1).ToList();
+                var user = users.FirstOrDefault(u => u.Name == primary) ?? CreateNewUser(primary);
+                followers.ForEach(f =>
+                {
+                    var followee = users.FirstOrDefault(u => u.Name == f.Trim()) ?? CreateNewUser(f);
+                    AddUser(user.Following, followee);
+                    AddUser(users, followee);
+                });
+                AddUser(users, user);
+            });
+            return users;
+        }
+
+        private static void AddUser(List<User> users, User user)
+        {
+            if (users.All(u => u.Name != user.Name))
+            {
+                users.Add(user);
+            }
+        }
+
+        private User CreateNewUser(string primary)
+        {
+            return new User
+            {
+                Name = primary
+            };
         }
 
         private string CreateTweet(string tweetLine)
@@ -46,5 +87,15 @@ namespace TwitterFeed
             var strings = tweetLine.Split(new [] {"> "}, StringSplitOptions.RemoveEmptyEntries);
             return $"\t@{strings[0]}: {strings[1]}";
         }
+    }
+
+    public class User
+    {
+        public User()
+        {
+            Following = new List<User>();
+        }
+        public string Name { get; set; }
+        public List<User> Following { get; }
     }
 }
